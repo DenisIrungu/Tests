8091c17d8cffb9de34fcefd999e8edd9eef0f61a
DenisIrungu <dennisirungu73@gmail.com>
2024-06-04 22:09:49 +0300

    RevisionProgress.py

--- RevisionProgress.py
+++ RevisionProgress.py
@@ -8,7 +8,7 @@ print("~                                 ~")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("")
 
-topics = ["System Architecture",
+revised_topics = ["System Architecture",
           "Memory and Storage",
           "Computer Networks",
           "Network Security",
@@ -19,14 +19,35 @@ topics = ["System Architecture",
           "Producing Robust Programs",
           "Boolean Logic",
           "Programming Languages and IDEs"]
-for topic in topics:
-    response = input(f"Have you complited the revision topic: {topic}? ")
-    if response.lower() == 'yes':
-        print(f" Good job") 
+
+no_of_topics_rev=0
+total_topics=len(revised_topics)
+
+
+
+for revised_topic in revised_topics:
+    response = input(f"Have you revised {revised_topic}? ")
+    if response.lower() =='y':
+        no_of_topics_rev+= 1
+        print ("Good")
         continue
-    elif response.lower() == 'no':
-        print(f" You need to revice this topic: {topic}")
+    elif response.lower() =='n':
+        print("You need to revise this topic")
         continue
     else:
-        print("Invalid Input")
+        print("Invalid input. Enter yes or no to continue")
         break
+
+        
+def prog_score():
+    prog_scores=((no_of_topics_rev)/(total_topics))*100
+    y=int(prog_scores)
+
+    print(f"Your score is {y}")
+    if y >60:
+        print(f"Your score is {y}. You qualify to seat for exams")
+    else:
+         print(f"Your score is:{y}. You don't qualify to saet for Exams ")
+    
+prog_score()
+        
